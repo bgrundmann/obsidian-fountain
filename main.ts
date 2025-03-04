68bee885aee87e4288eb5c93cabee2ed7f72a7a1
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2025-03-04 09:35:41 +0100

    Basic ability to create new files.

--- main.ts
+++ main.ts
@@ -6,6 +6,19 @@ export default class FountainPlugin extends Plugin {
     // Register our custom view and associate it with .fountain files
     this.registerView(VIEW_TYPE_FOUNTAIN, (leaf) => new FountainView(leaf));
     this.registerExtensions(["fountain"], VIEW_TYPE_FOUNTAIN);
+    this.addRibbonIcon("square-pen", "New fountain document", (evt) => {
+      let destination = "Untitled.fountain";
+      const activeFile = this.app.workspace.getActiveFile();
+      if (activeFile !== null) {
+        if (activeFile?.parent) {
+          destination = `${activeFile.parent.path}/Untitled.fountain`;
+        }
+      }
+      this.app.vault.create(destination, "").then((newFile) => {
+        const leaf = this.app.workspace.getLeaf(false);
+        leaf.openFile(newFile);
+      });
+    });
   }
   async onunload() {
     // Is there a way to "unregister" the View and extensions?
